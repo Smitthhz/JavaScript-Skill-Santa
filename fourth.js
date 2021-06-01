1ab03fd052482b1e3e93ad0374b1a9ee3e2a0cc6
Gaurav Varadkar <66435294+Smitthhz@users.noreply.github.com>
2021-06-01 21:12:48 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,10 @@
+var num=prompt("Enter a integer value: - ");
+let range;
+if(num>=50 && num<=100)
+{
+    document.write("The Entered integer " +num+ " is withing range!");
+   }
+      else{
+    document.write("Out of Bound");
+      }
+      
