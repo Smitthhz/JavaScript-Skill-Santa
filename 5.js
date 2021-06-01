9ae0bdf5b25fa5e73cd6185eb60058d2433f3b32
Gaurav Varadkar <66435294+Smitthhz@users.noreply.github.com>
2021-06-01 21:52:43 +0530

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,12 @@
+const string = prompt("Enter a String: - ");
+function reverseString(str) 
+{
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) 
+    {
+        newString += str[i];
+    }
+    return newString;
+}
+const result = reverseString(string);
+document.write("Reverse of entered String is: - " +result);
